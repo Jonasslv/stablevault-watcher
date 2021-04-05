836335b105deaf92be2940c7c08635e3e7b72459
Abdülkadir Poyraz <kadirpoyraz96@gmail.com>
2021-04-06 00:24:36 +0300

    refactor and add current reserves and tvl to each message

--- index.js
+++ index.js
@@ -1,183 +1,85 @@
 require('dotenv').config()
 const Web3 = require('web3');
-const rtrim = require('rtrim');
 const Discord = require('discord.js');
+const stableVaultAbi = require('./stablevault-abi.json');
+const utils = require('./utils');
+const rtrim = require('rtrim');
 
 const discord = new Discord.Client();
 const web3 = new Web3(process.env.WS_URL);
+const stableVaultContract = new web3.eth.Contract(stableVaultAbi, process.env.STABLEVAULT_CONTRACT_ADDRESS);
 
-const subscribeToSwaps = function () {
-    web3.eth.subscribe('logs', {
-        address: process.env.STABLEVAULT_CONTRACT_ADDRESS,
-        topics: [
-            web3.eth.abi.encodeEventSignature('TokenSwap(address,uint256,uint256,uint128,uint128)')
-        ]
-    }, function (error, result) {
-        const transactionHash = result.transactionHash;
-        const {tokensSold, tokensBought, soldId, boughtId} = web3.eth.abi.decodeLog(
-            [
-                {
-                    type: 'address',
-                    name: 'buyer',
-                    indexed: true,
-                },
-                {
-                    type: 'uint256',
-                    name: 'tokensSold'
-                },
-                {
-                    type: 'uint256',
-                    name: 'tokensBought'
-                },
-                {
-                    type: 'uint128',
-                    name: 'soldId'
-                },
-                {
-                    type: 'uint128',
-                    name: 'boughtId'
-                },
-            ],
-            result.data,
-            result.topics.slice(1),
-        );
-
-        const message = `\`\`\`Swap ${normalizeFloat(web3.utils.fromWei(tokensSold, getTokenUnitById(soldId)), 2)} ${getTokenById(soldId)} for ${normalizeFloat(web3.utils.fromWei(tokensBought, getTokenUnitById(boughtId)), 2)} ${getTokenById(boughtId)} \nTransaction: ${transactionHash}\`\`\``;
-        notifyDiscord(message);
+const subscribeToSwaps = async () => {
+    stableVaultContract.events.TokenSwap(null, async (error, event) => {
+        const message = await buildMessage(utils.operationTypes.swap, event.transactionHash, event.returnValues);
+        await notifyDiscord(message);
     });
 }
 
-const subscribeToAdds = function () {
-    web3.eth.subscribe('logs', {
-        address: process.env.STABLEVAULT_CONTRACT_ADDRESS,
-        topics: [
-            web3.eth.abi.encodeEventSignature('AddLiquidity(address,uint256[],uint256[],uint256,uint256)')
-        ]
-    }, function (error, result) {
-        const transactionHash = result.transactionHash;
-        const {tokenAmounts} = web3.eth.abi.decodeLog(
-            [
-                {
-                    type: 'address',
-                    name: 'provider',
-                    indexed: true,
-                },
-                {
-                    type: 'uint256[]',
-                    name: 'tokenAmounts'
-                },
-                {
-                    type: 'uint256[]',
-                    name: 'fees'
-                },
-                {
-                    type: 'uint256',
-                    name: 'invariant'
-                },
-                {
-                    type: 'uint256',
-                    name: 'lpTokenSupply'
-                },
-            ],
-            result.data,
-            result.topics.slice(1),
-        );
+const subscribeToAdds = async () => {
+    stableVaultContract.events.AddLiquidity(null, async (error, event) => {
+        const message = await buildMessage(utils.operationTypes.add, event.transactionHash, event.returnValues);
+        await notifyDiscord(message);
+    });
+}
 
-        let message = '```Add';
+const subscribeToRemoves = async () => {
+    stableVaultContract.events.RemoveLiquidity(null, async (error, event) => {
+        const message = await buildMessage(utils.operationTypes.remove, event.transactionHash, event.returnValues);
+        await notifyDiscord(message);
+    });
+}
 
-        for (const [tokenId, amount] of Object.entries(tokenAmounts)) {
+const buildMessage = async (type, transactionHash, data) => {
+    let message = `\`\`\`${type}`;
+
+    if (type === utils.operationTypes.swap) {
+        message += `${utils.normalizeFloat(web3.utils.fromWei(data.tokensSold, utils.getTokenUnitById(data.soldId)), 2)} ${utils.getTokenById(data.soldId)} for ${utils.normalizeFloat(web3.utils.fromWei(data.tokensBought, utils.getTokenUnitById(data.boughtId)), 2)} ${utils.getTokenById(data.boughtId)}`;
+    } else {
+        for (const [tokenId, amount] of Object.entries(data.tokenAmounts)) {
             if (amount !== '0') {
-                message += ` ${normalizeFloat(web3.utils.fromWei(amount, getTokenUnitById(tokenId)), 2)} ${getTokenById(tokenId)} +`;
+                message += ` ${utils.normalizeFloat(Web3.utils.fromWei(amount, utils.getTokenUnitById(tokenId)), 2)} ${utils.getTokenById(tokenId)} +`;
             }
         }
 
         message = rtrim(message, '+');
-        message += `\nTransaction: ${transactionHash}\`\`\``;
+    }
 
-        notifyDiscord(message);
-    });
+    message += `\nTransaction: ${transactionHash}`;
+
+    message += `\nReserves: `;
+    const reserves = await getReserves();
+    let tvl = 0;
+
+    for (const [tokenId, reserve] of Object.entries(reserves)) {
+        const reserveWithDecimals = Web3.utils.fromWei(reserve, utils.getTokenUnitById(tokenId));
+        message += ` ${utils.normalizeFloat(reserveWithDecimals, 2)} ${utils.getTokenById(tokenId)} +`;
+        tvl +=  parseInt(reserveWithDecimals);
+    }
+
+    message = rtrim(message, '+');
+    message += `\nTVL: $${tvl}`;
+    message += '```';
+
+    return message;
 }
 
-const subscribeToRemoves = function () {
-    web3.eth.subscribe('logs', {
-        address: process.env.STABLEVAULT_CONTRACT_ADDRESS,
-        topics: [
-            web3.eth.abi.encodeEventSignature('RemoveLiquidity(address,uint256[],uint256)')
-        ]
-    }, function (error, result) {
-        const transactionHash = result.transactionHash;
-        const {tokenAmounts} = web3.eth.abi.decodeLog(
-            [
-                {
-                    type: 'address',
-                    name: 'provider',
-                    indexed: true,
-                },
-                {
-                    type: 'uint256[]',
-                    name: 'tokenAmounts'
-                },
-                {
-                    type: 'uint256',
-                    name: 'lpTokenSupply'
-                },
-            ],
-            result.data,
-            result.topics.slice(1),
-        );
-
-        let message = '```Remove';
-
-        for (const [tokenId, amount] of Object.entries(tokenAmounts)) {
-            if (amount !== '0') {
-                message += ` ${normalizeFloat(web3.utils.fromWei(amount, getTokenUnitById(tokenId)), 2)} ${getTokenById(tokenId)} +`;
-            }
-        }
-
-        message = rtrim(message, '+');
-        message += `\nTransaction: ${transactionHash}\`\`\``;
-
-        notifyDiscord(message);
-    });
-}
-
-notifyDiscord = async function (message) {
+const notifyDiscord = async (message) => {
     const channel = await discord.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
     await channel.send(message);
 }
 
-getTokenUnitById = function (id) {
-    const idToUnitMapping = {
-        0: 'mwei', // USDT
-        1: 'ether', // BUSD
-        2: 'ether', // DAI
-    };
+const getReserves = async () => {
+    const reserves = {};
 
-    return idToUnitMapping[id];
-}
-
-getTokenById = function (id) {
-    const idToTokenMapping = {
-        0: 'USDT',
-        1: 'BUSD',
-        2: 'DAI',
-    };
-
-    return idToTokenMapping[id];
-}
-
-normalizeFloat = function (numberStr, decimals) {
-    const indexOfDot = numberStr.indexOf('.');
-    let normalized = numberStr;
-
-    if (indexOfDot !== -1) {
-        normalized = `${numberStr.substring(0, indexOfDot)}.${numberStr.substr(indexOfDot + 1, decimals)}`;
+    for (const [id] of Object.entries(utils.tokens)) {
+        reserves[id] = await stableVaultContract.methods.getTokenBalance(id).call();
     }
 
-    return normalized;
+    return reserves;
 }
 
-const start = async function () {
+const start = async () => {
     await discord.login(process.env.DISCORD_BOT_TOKEN);
     await discord.channels.fetch(process.env.DISCORD_CHANNEL_ID);
     subscribeToSwaps();
